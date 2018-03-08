a544b7db315e7531d2ce39bc33d062dcade08154
Antonella Morittu <morittuantonella@gmail.com>
2018-03-08 11:31:25 +0100

    set correctly calculate method in home.ts file

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,14 +11,15 @@ export class HomePage {
   user: any = {};
 
   constructor(
-  public navCtrl: NavController,
-  public person: PersonProvider
+    public navCtrl: NavController,
+    public person: PersonProvider,
+    private performanceData: PerformanceDataProvider
   ) {
-  this.user = { distance: 1000, age: 20, gender: 'female' };
+    this.user = { distance: 1000, age: 20, gender: 'female' };
   }
 
-  calculate(user) {
-    this.person.doAssessment(user.distance);
+  calculate() {
+    this.person.doAssessment(this.user.distance);
     this.performanceData
       .saveData({ performance_data: { data: { message: this.person.assessmentMessage } } })
       .subscribe(data => console.log(data));
